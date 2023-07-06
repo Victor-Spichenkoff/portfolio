47176f0cb91c275d054069f7257bd42ebced7244
Victor Spichenkoff Santana <victorspich13@gmail.com>
2023-07-05 21:37:10 -0300

    First Changes

---
+++ src/components/templates/Header.tsx
@@ -0,0 +1,15 @@
+import s from '@styles/template/header.module.css'
+
+interface Props {
+
+}
+
+export default function Header(prosp: Props) {
+    
+    return (
+        <div className={s.header}>
+        <header></header>
+        
+        </div>
+    )
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/templates/Header'
+
+interface Props {
+
+}
+
+export default function Index() {
+    return (
+        <>
+        <Header />
+        
+        
+        </>
+    )
+}
