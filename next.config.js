1e791faf04322e6631a8dc396f276772bb1dae5b
Victor-Spichenkoff <victorspich13@gmail.com>
2024-06-10 20:45:15 -0300

    remove 404 from pre

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  exportPathMap: function () {
+    return {
+      '/404': { page: '/404', asPath: '/404' },
+    };
+  },
 }
 
 module.exports = nextConfig
