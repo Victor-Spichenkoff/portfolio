af06fc5128c906ab1a6250d78e64a02150314a10
Victor-Spichenkoff <victorspich13@gmail.com>
2024-08-02 21:36:17 -0300

    Visual fixes

--- src/components/functions/fy.tsx
+++ src/components/functions/fy.tsx
@@ -11,16 +11,16 @@ let stop = false
 
 export default function Fy() {
 
-    const [projects, setProjects] = useState([{name:'', user:{}}])
-    const [page, setPage] = useState(0)
-    const [showLoading, setShowLoading] = useState(true)
-    const [stop, setStop] = useState(false)
+  const [projects, setProjects] = useState([{ name: '', user: {} }])
+  const [page, setPage] = useState(0)
+  const [showLoading, setShowLoading] = useState(true)
+  const [stop, setStop] = useState(false)
 
 
-  async function getProjectsHere (page?:number) {
-      if(stop) return
-      // let pageUse = page >= 0 ? page-1 : page
-      const user = getStoragedUser()
+  async function getProjectsHere(page?: number) {
+    if (stop) return
+    // let pageUse = page >= 0 ? page-1 : page
+    const user = getStoragedUser()
 
     function verifySentinelaExit() {
       const sentinela = document.getElementById('sentinela')
@@ -30,78 +30,79 @@ export default function Fy() {
 
     verifySentinelaExit()
 
-      axios.get(`${baseUrl}/project/fy/${user.id}?page=${page}`)
-          .then(res => {
-            console.log(page, res.data)
-              if(res.data.length==0)  {
-                  setStop(true)
-                  setShowLoading(false)
-                  return 
-              }
-              else{
-                // console.log(projects)
-                // console.log(res.data)
-                if(page==0) {
-                  setProjects(res.data)
-                } else{
-                  setProjects((old:any) => {
-                    try{
-                      if(old[page*4].id == res.data[0].id) return [...old]
-                    } catch(e) {}
-                    return [...old, ...res.data]
-                  })
-                }
-              }
-          })
-          .catch(res => console.log('erro no get'))
+    axios.get(`${baseUrl}/project/fy/${user.id}?page=${page}`)
+      .then(res => {
+        console.log(page, res.data)
+        if (res.data.length == 0) {
+          setStop(true)
+          setShowLoading(false)
+          return
+        }
+        else {
+          // console.log(projects)
+          // console.log(res.data)
+          if (page == 0) {
+            setProjects(res.data)
+          } else {
+            setProjects((old: any) => {
+              try {
+                if (old[page * 4].id == res.data[0].id) return [...old]
+              } catch (e) { }
+              return [...old, ...res.data]
+            })
+          }
+        }
+      })
+      .catch(res => console.log('erro no get'))
   }
 
 
-    useEffect(()=>{
-      // setInterval(()=>{
+  useEffect(() => {
+    // setInterval(()=>{
 
 
-        let observer = new IntersectionObserver((entries)=>{
-          if(entries.some((entry) => entry.intersectionRect)) {
-            setPage(old => {
-              getProjectsHere(old)
-              return old+1
-            })
-            console.log('nhe')
-          }
+    let observer = new IntersectionObserver((entries) => {
+      if (entries.some((entry) => entry.intersectionRect)) {
+        setPage(old => {
+          getProjectsHere(old)
+          return old + 1
         })
+      }
+    })
+
+    try {
+      observer.observe(document.getElementById('sentinela'))
+    } catch (e) { console.log('erro no observer') }
+
+    if (!showLoading) {
+      return observer.disconnect()
+    }
+    // }, 1000)
+  }, [])
 
 
-        try{
-          observer.observe(document.getElementById('sentinela'))
-        } catch(e) {console.log('erro no observer')}
-    
-        if(!showLoading) {
-          return  observer.disconnect()
-        }
-      // }, 1000)
-    }, [])
 
-    
-
-    return (
+  return (
     <div className="fy-home">
-        <h2 className='discovery'>Discovery</h2>
-        <hr />
-        
-        <div className='fy'>
-            {projects.map((project) => {
-              return (
-                <FyProject project={project} key={Math.random()*Math.random()}></FyProject>
-              )
-            })}
-        </div>
+      <div id="limiter-fy">
+      <h2 className='discovery'>Discovery</h2>
+      <hr />
 
-        {showLoading ? (
-          <div id="sentinela">
-            <img src="https://metodosupera.com.br/supera-quiz-soft-skills/src/images/loading.gif" alt='Loading...' />
-          </div>
-        ) : (<div className="no-more-projects"><p>That&apos;s all</p></div>)}
+      <div className='fy'>
+        {projects.map((project) => {
+          return (
+            <FyProject project={project} key={Math.random() * Math.random()}></FyProject>
+          )
+        })}
+      </div>
+
+      {showLoading ? (
+        <div id="sentinela">
+          <img src="https://metodosupera.com.br/supera-quiz-soft-skills/src/images/loading.gif" alt='Loading...' />
+        </div>
+      ) : (<div className="no-more-projects"><p>That&apos;s all</p></div>)}
+
+      </div>
     </div>
-    )
+  )
 }
